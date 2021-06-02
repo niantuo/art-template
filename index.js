a3fb9c54c591ef33de438cbc2ce79fadb68e555d
tuon <976056042@qq.com>
2021-06-02 17:13:44 +0800

    fix: react项目引入异常的问题

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const template = require('./lib/index');
 const extension = require('./lib/extension');
 
 template.extension = extension;
-require.extensions[template.defaults.extname] = extension;
-
+if (require.extensions) {
+    require.extensions[template.defaults.extname] = extension;
+}
 module.exports = template;
